b2851a975a5b0727976ce3c8b8a3fea386715975
bret-gibson <bret.gibson@gmail.com>
2020-08-31 23:37:51 -0400

    added palindrome solution with queue

--- queue-palindrome.js
+++ queue-palindrome.js
@@ -29,3 +29,53 @@ class Queue {
     return toString;
   }
 }
+
+function isPalindrome(string) {
+  let queue = new Queue();
+  let stringArr = string.split("");
+  let reversedString = "";
+
+  // put all letters into the queue - starting from the back this time
+  // due to a queue's first in first out design
+  for (let i = string.length - 1; i >= 0; i--) {
+    queue.enqueue(string[i]);
+  }
+
+  while (queue.isEmpty() === false) {
+    reversedString += queue.dequeue();
+  }
+
+  if (reversedString === string) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// testing odd number of letters
+if (isPalindrome("racecar") === true) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
+
+// testing even number of letters
+if (isPalindrome("abbbba") === true) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
+
+// testing odd non-palindrome
+if (isPalindrome("hello") === true) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
+
+// testing even non-palindrome
+if (isPalindrome("code") === true) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
